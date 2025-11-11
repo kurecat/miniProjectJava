c3c2b663075336bdc1904acce63dae120835c363
kurecat <ochant777@gmail.com>
2025-11-11 17:26:17 +0900

    asd

--- miniProjectJava/src/main/java/mini_Project/web_board/dao/PostDao.java
+++ miniProjectJava/src/main/java/mini_Project/web_board/dao/PostDao.java
@@ -13,17 +13,17 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 @Slf4j
 public class PostDao {
-    private final JdbcTemplate jdbc;
-    private final MemberRes memberRes;
-    private final CategoryRes categoryRes;
-
-    public Long create(PostCreateReq p){
-        @Language("SQL")
-        String sql = """
-                     INSERT INTO posts(id, member_id, title, contnet, category_id, view_count, recommendations_count,created_at)
-                     VALUES (seq_posts.NEXTVAL, ?, ?, ?, ?, ?, ?, SYSDATE)
-                     """;
-        jdbc.update(sql, memberRes.getId(), p.getTitle(), p.getContent(), categoryRes.getId(), 0, 0);
-        return jdbc.queryForObject("SELCET seq_posts.CURRVAL FROM daul", Long.class);
-    }
+//    private final JdbcTemplate jdbc;
+//    private final MemberRes memberRes;
+//    private final CategoryRes categoryRes;
+//
+//    public Long create(PostCreateReq p){
+//        @Language("SQL")
+//        String sql = """
+//                     INSERT INTO posts(id, member_id, title, contnet, category_id, view_count, recommendations_count,created_at)
+//                     VALUES (seq_posts.NEXTVAL, ?, ?, ?, ?, ?, ?, SYSDATE)
+//                     """;
+//        jdbc.update(sql, memberRes.getId(), p.getTitle(), p.getContent(), categoryRes.getId(), 0, 0);
+//        return jdbc.queryForObject("SELCET seq_posts.CURRVAL FROM daul", Long.class);
+//    }
 }
